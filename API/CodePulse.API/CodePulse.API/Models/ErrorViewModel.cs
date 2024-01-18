269435df366d6fea4b4b1e98482a3350c5b9341b
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-18 22:22:17 +0400

    upload initial project

--- API/CodePulse.API/CodePulse.API/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodePulse.API.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
