e94cb838938637281bbba6f1b31f754fc95a4661
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-19 16:01:31 +0400

    create new controller

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")] // <= https://localhost:xxxxxx/api/caregories
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+       
+    }
+}
+
