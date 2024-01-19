b9a6c0ec8fd52a03d750c1f4c42bce58508e2032
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-19 15:54:03 +0400

    Run EntityFramework Core Migration

--- API/CodePulse.API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
+++ API/CodePulse.API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6250a9eb7aa813d0d84dc118966c7830f2db8d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
