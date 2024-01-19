1df54dcde7610eeb4dedb07accf7767526d03b79
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-19 14:55:50 +0400

    Create DbContext Class

---
+++ API/CodePulse.API/CodePulse.API/Models/Domain/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CodePulse.API.Models.Domain
+{
+	public class Category
+	{
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string UrlHandle { get; set; }
+    }
+}
+
