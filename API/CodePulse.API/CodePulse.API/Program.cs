f6250a9eb7aa813d0d84dc118966c7830f2db8d6
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-19 15:20:30 +0400

    injecting DbContext into application

--- API/CodePulse.API/CodePulse.API/Program.cs
+++ API/CodePulse.API/CodePulse.API/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using CodePulse.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Injecting DbContext 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+});
+
 
 var app = builder.Build();
 
