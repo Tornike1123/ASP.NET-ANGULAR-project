e86b569d322921ef3dc5ac920bad70385b46e1e1
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-24 22:20:12 +0400

    environment files

--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -3,6 +3,7 @@ import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +12,15 @@ export class CategoryService {
   constructor(private http: HttpClient) {}
 
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>('https://localhost:7058/api/categories', model);
+    return this.http.post<void>(
+      `${environment.apiBaseUrl}/api/categories`,
+      model
+    );
   }
 
   getAllCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>('https://localhost:7058/api/Categories');
+    return this.http.get<Category[]>(
+      `${environment.apiBaseUrl}/api/Categories`
+    );
   }
 }
