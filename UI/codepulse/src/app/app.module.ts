fffb01d834aa63eb3a3a2e24bddd751500b20e07
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-24 20:08:15 +0400

    create add category component amd routing

--- UI/codepulse/src/app/app.module.ts
+++ UI/codepulse/src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { AddCategoryComponent } from './features/category/add-category/add-category.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CategoryListComponent
+    CategoryListComponent,
+    AddCategoryComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
