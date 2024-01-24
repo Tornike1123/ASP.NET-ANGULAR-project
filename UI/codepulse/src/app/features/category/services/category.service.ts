b8377b5d24f4422d74593b578dd8cd0757087e8b
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-24 22:13:15 +0400

    Create UI Seeervice method to Get All Categories

--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,9 @@ export class CategoryService {
 
   addCategory(model: AddCategoryRequest): Observable<void> {
     return this.http.post<void>('https://localhost:7058/api/categories', model);
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>('https://localhost:7058/api/Categories');
   }
 }
